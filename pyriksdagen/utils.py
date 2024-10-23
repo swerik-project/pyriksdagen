4b530d814f8467cb8c2b77ea0b8fccc7f3d260a5
Bob Borges <robert.d.borges@gmail.com>
2024-10-23 12:39:09 +0200

    feat: add compiled db to get_data_location() opts

--- pyriksdagen/utils.py
+++ pyriksdagen/utils.py
@@ -404,6 +404,7 @@ def get_data_location(partition):
     d["records"] = os.environ.get("RECORDS_PATH", "data")
     d["motions"] = os.environ.get("MOTIONS_PATH", "data")
     d["metadata"] = os.environ.get("METADATA_PATH", "data")
+    d["metadata_db"] = os.environ.get("METADATA_DB", "data")               # path to csv or pkl of compiled Corpus()
     d["interpellations"] = os.environ.get("INTERPELLATIONS_PATH", "data")
     return d[partition]
 

29d8a56172b07115e345bfa1f5659044ba924eca
ninpnin <vainoyrjanainen@icloud.com>
2024-12-13 11:26:21 +0100

    refactor: better variable names

--- pyriksdagen/refine.py
+++ pyriksdagen/refine.py
@@ -374,33 +374,33 @@ def detect_date(root, metadata):
     for ix, elem_tuple in enumerate(list(elem_iter(root))):
         tag, elem = elem_tuple
         if tag == "note" and type(elem.text) == str and len(" ".join(elem.text.split()))  < 50:
-            matches = re.search(expression, elem.text)
-            matches2 = re.search(expression2, elem.text)
-            matches3 = re.search(expression3, elem.text)
+            weekday_date_year = re.search(expression, elem.text)
+            weekday_and_date = re.search(expression2, elem.text)
+            date_and_year = re.search(expression3, elem.text)
 
             # Dates with the year included, surely date
-            if matches is not None:
+            if weekday_date_year is not None:
                 if elem.attrib.get("type") != "title":
                     elem.attrib["type"] = "date"
-                datestr = matches.group(1) + " " + matches.group(2) + " " + matches.group(3)
+                datestr = weekday_date_year.group(1) + " " + weekday_date_year.group(2) + " " + weekday_date_year.group(3)
                 date = dateparser.parse(datestr, languages=["sv"])
                 if date is not None and not _is_sjukbetyg(elem):
                     if date.year in protocol_years:
                         number_dates.add(date)
 
             # Dates with the year included, though unsure if protocol date
-            elif matches3 is not None and not _is_sjukbetyg(elem):
-                datestr = matches3.group()
+            elif date_and_year is not None and not _is_sjukbetyg(elem):
+                datestr = date_and_year.group()
                 date = dateparser.parse(datestr, languages=["sv"])
                 if date is not None:
                     if date.year in protocol_years:
                         dates.add(date)
 
             # Dates without a year
-            elif matches2 is not None and not _is_sjukbetyg(elem):
+            elif weekday_and_date is not None and not _is_sjukbetyg(elem):
                 if elem.attrib.get("type") != "title":
                     elem.attrib["type"] = "date"
-                datestr = matches2.group(1) + " " + matches2.group(2)
+                datestr = weekday_and_date.group(1) + " " + weekday_and_date.group(2)
                 yearless.add(datestr)
 
     if len(dates) > 0:
